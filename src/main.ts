e38ed910253fa0c6172b609da64e2e72a9e7baf5
peymanprd <dev.pirzadeh@gmail.com>
2023-11-19 15:52:09 +0330

    edit app template

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,17 @@
 import './style.css'
-import './sass/index.sass'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
+// import './sass/index.sass'
 import { setupCounter } from './counter.ts'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+const app = document.querySelector<HTMLDivElement>('#app')!
+
+app.innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
+    <h1>PlayHouse</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
   </div>
 `
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const counter = document.querySelector<HTMLButtonElement>('#counter')!
+setupCounter(counter)
